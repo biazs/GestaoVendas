0e60a6a2135f6d903e6c205e4fe3b6c6d1725cd6
Bia Zoroastro <fabianazs@gmail.com>
2020-07-05 18:46:44 -0300

    Identity

---
+++ GestaoVendas/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using GestaoVendas.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(GestaoVendas.Areas.Identity.IdentityHostingStartup))]
+namespace GestaoVendas.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
