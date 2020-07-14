5cc1b431eacab87e6ff89c1835a5fe3ad3f82189
Bia Zoroastro <fabianazs@gmail.com>
2020-07-13 23:16:50 -0300

    Projeto de testes unitários

---
+++ GestaoVendasTestes/UnitTestModels.cs
@@ -0,0 +1,19 @@
+using GestaoVendas.Models.Dao;
+using Xunit;
+
+namespace GestaoVendasTestes
+{
+    public class UnitTestModels
+    {
+
+        [Fact]
+        public void CheckTypeListaProdutos()
+        {
+            DaoProduto objProduto = new DaoProduto();
+
+            // var lista = objProduto.ListarTodosProdutos(??);
+            // Assert.IsType<List<Produto>>(lista);
+
+        }
+    }
+}
