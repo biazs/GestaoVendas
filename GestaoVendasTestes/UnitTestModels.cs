5e32c3ecce4522b7a217b244764a504e10b14e76
Bia Zoroastro <fabianazs@gmail.com>
2020-09-21 16:49:48 -0300

    Teste unitário

--- GestaoVendasTestes/UnitTestModels.cs
+++ GestaoVendasTestes/UnitTestModels.cs
@@ -1,16 +1,33 @@
+using GestaoVendas.Data;
+using GestaoVendas.Models;
+using GestaoVendas.Models.Dao;
+using System.Collections.Generic;
 using Xunit;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace GestaoVendasTestes
 {
+    
     public class UnitTestModels
     {
+        //[Fact]
+        //public void CheckTypeListaProdutos()
+        //{
+        //    //DaoProduto daoProduto = new DaoProduto();
+        //    //List<Produto> lista = daoProduto.ListarTodosProdutos();
+        //    //Assert.IsType<List<Produto>>(lista);
+        //}
 
         [Fact]
-        public void CheckTypeListaProdutos()
-        {
-            //var lista = _daoProduto.ListarTodosProdutos();
-            //Assert.IsType<List<Produto>>(lista);
+        public void ValidaQuantidadeProduto()
+        {            
+            int quantidade = 2;            
+            Produto produto = new Produto();
+            produto.Quantidade = quantidade;
 
+            // Assert            
+            Assert.Equal(quantidade, produto.Quantidade);
+           
         }
     }
 }
